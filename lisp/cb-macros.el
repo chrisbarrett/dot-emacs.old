31c713d41fc889bd0182d4b0ee41e33413086d4c
Chris Barrett <chris.d.barrett@me.com>
2013-04-03 01:43:31 +1300

    Tweak macros

--- lisp/cb-macros.el
+++ lisp/cb-macros.el
@@ -7,7 +7,7 @@
   (and (fboundp symbol)
        (eq (car (symbol-function symbol)) 'macro)))
 
-(defmacro defmacro-safe (symbol arglist &rest body)
+(defmacro cb:defmacro-safe (symbol arglist &rest body)
   "Define the given macro only if it is not already defined."
   (declare (doc-string 3) (indent defun))
   (cl-assert (symbolp symbol))
@@ -15,14 +15,14 @@
   `(unless (macro-boundp ',symbol)
      (cl-defmacro ,symbol ,arglist ,@body)))
 
-(defmacro-safe when-let ((var form) &rest body)
+(cb:defmacro-safe when-let ((var form) &rest body)
   "Execute BODY forms with bindings only if FORM evaluates to a non-nil value."
   (declare (indent 1))
   `(let ((,var ,form))
      (when ,var
        ,@body)))
 
-(defmacro-safe if-let ((var form) then &rest else)
+(cb:defmacro-safe if-let ((var form) then &rest else)
   "Execute THEN form with bindings if FORM evaluates to a non-nil value,
 otherwise execute ELSE forms without bindings."
   (declare (indent 1))
@@ -35,6 +35,5 @@ to the hook function are bound to the symbol 'args'."
   (declare (indent 1) (doc-string 2))
   `(add-hook ,hook (lambda (&rest args)
                      ,@(cons docstring body))))
-
 
 (provide 'cb-macros)
