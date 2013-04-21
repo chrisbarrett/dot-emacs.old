40c9ac1bffb2c44273a192b31ebbfbe7af2fd38c
Chris Barrett <chris.d.barrett@me.com>
2013-04-21 21:11:59 +1200

    Use ido for sudo-edit command

--- lisp/cb-commands.el
+++ lisp/cb-commands.el
@@ -79,10 +79,15 @@ If this buffer is a member of `kill-buffer-ignored-list, bury it rather than kil
     (kill-buffer (current-buffer))))
 
 (defun sudo-edit (&optional arg)
-  "Edit a file with elevated privileges."
-  (interactive "p")
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
   (if (or arg (not buffer-file-name))
-      (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
 (defun cb:hide-dos-eol ()
