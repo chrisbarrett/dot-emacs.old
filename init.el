08f3ec0ec3610b5b83e0a331d31f6e737df63e7f
Chris Barrett <chris.d.barrett@me.com>
2013-04-26 21:15:42 +1200

    Change ac-quick-help-delay -> 0.25

--- init.el
+++ init.el
@@ -698,7 +698,7 @@
      ac-auto-show-menu t
      ac-dwim t
      ac-use-menu-map t
-     ac-quick-help-delay 1
+     ac-quick-help-delay 0.25
      ac-quick-help-height 60
      ac-disable-inline t
      ac-show-menu-immediately-on-auto-complete t
