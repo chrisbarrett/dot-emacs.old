f7c3c9d82e43ca5fcf2ca6c3f3a5c48540dfb554
Chris Barrett <chris.d.barrett@me.com>
2013-07-18 01:12:01 +1200

    Add command to archive completed tasks

--- lisp/cb-org.el
+++ lisp/cb-org.el
@@ -309,6 +309,9 @@ With prefix argument ARG, show the file and move to the tasks tree."
   :commands (org-agenda org-agenda-list)
   :init
   (progn
+    (defvar org-my-archive-expiry-days 2
+      "The number of days after which a completed task should be auto-archived.
+This can be 0 for immediate, or a floating point value.")
 
     (declare-modal-view org-agenda-list)
 
@@ -366,7 +369,36 @@ With prefix argument ARG, show the file and move to the tasks tree."
       ;; Show usage. Run on timer so other messages are buried.
       (run-with-timer
        0.1 nil
-       (lambda () (message "<C-c C-c> to commit changes, <C-c C-k> to abort."))))))
+       (lambda () (message "<C-c C-c> to commit changes, <C-c C-k> to abort."))))
+
+    ;;;; Archiving
+
+    (defun cb-org:archive-done-tasks ()
+      (interactive)
+      (save-excursion
+        (goto-char (point-min))
+        (let ((done-regexp
+               (concat "\\* \\(" (regexp-opt org-done-keywords) "\\) "))
+              (state-regexp
+               (concat "- State \"\\(" (regexp-opt org-done-keywords)
+                       "\\)\"\\s-*\\[\\([^]\n]+\\)\\]")))
+          (while (re-search-forward done-regexp nil t)
+            (let ((end (save-excursion
+                         (outline-next-heading)
+                         (point)))
+                  begin)
+              (goto-char (line-beginning-position))
+              (setq begin (point))
+              (when (re-search-forward state-regexp end t)
+                (let* ((time-string (match-string 2))
+                       (when-closed (org-parse-time-string time-string)))
+                  (if (>= (time-to-number-of-days
+                           (time-subtract (current-time)
+                                          (apply #'encode-time when-closed)))
+                          org-my-archive-expiry-days)
+                      (org-archive-subtree)))))))))
+
+    (defalias 'archive-done-tasks 'cb-org:archive-done-tasks)))
 
 (provide 'cb-org)
 
