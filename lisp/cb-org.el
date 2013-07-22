0e0815db52b4c104260797e3e66b6b8a6594c398
Chris Barrett <chris.d.barrett@me.com>
2013-07-22 20:47:32 +1200

    Ensure emacs lisp is loaded for org babel

--- lisp/cb-org.el
+++ lisp/cb-org.el
@@ -107,6 +107,7 @@ With prefix argument ARG, show the file and move to the tasks tree."
 
     (org-babel-do-load-languages
      'org-babel-load-languages '((python . t)
+                                 (emacs-lisp . t)
                                  (ruby . t)
                                  (clojure . t)
                                  (haskell . t)))
