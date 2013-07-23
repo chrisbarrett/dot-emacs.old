c5b6738d03d1b306df46237a4b965c6e486e8b9b
Chris Barrett <chris.d.barrett@me.com>
2013-07-24 00:00:54 +1200

    Simpler implementation of archiving done tasks.

--- lisp/cb-org.el
+++ lisp/cb-org.el
@@ -432,32 +432,12 @@ This can be 0 for immediate, or a floating point value.")
 
     ;;;; Archiving
 
-    (defun cb-org:archive-done-tasks ()
+    (defun org-archive-done-tasks ()
       (interactive)
-      (save-excursion
-        (goto-char (point-min))
-        (let ((done-regexp
-               (concat "\\* \\(" (regexp-opt org-done-keywords) "\\) "))
-              (state-regexp
-               (concat "- State \"\\(" (regexp-opt org-done-keywords)
-                       "\\)\"\\s-*\\[\\([^]\n]+\\)\\]")))
-          (while (re-search-forward done-regexp nil t)
-            (let ((end (save-excursion
-                         (outline-next-heading)
-                         (point)))
-                  begin)
-              (goto-char (line-beginning-position))
-              (setq begin (point))
-              (when (re-search-forward state-regexp end t)
-                (let* ((time-string (match-string 2))
-                       (when-closed (org-parse-time-string time-string)))
-                  (if (>= (time-to-number-of-days
-                           (time-subtract (current-time)
-                                          (apply #'encode-time when-closed)))
-                          org-my-archive-expiry-days)
-                      (org-archive-subtree)))))))))
+      (atomic-change-group
+        (org-map-entries 'org-archive-subtree "/DONE" 'file)))
 
-    (defalias 'archive-done-tasks 'cb-org:archive-done-tasks)))
+    (defalias 'archive-done-tasks 'org-archive-done-tasks)))
 
 (provide 'cb-org)
 
