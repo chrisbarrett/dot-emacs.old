8849cac10f1c0caa92bb48a63917d4e9c457719e
Chris Barrett <chris.d.barrett@me.com>
2013-07-24 15:11:47 +1200

    Add calc to list of org babel languages

--- lisp/cb-org.el
+++ lisp/cb-org.el
@@ -127,6 +127,7 @@ With prefix argument ARG, show the file and move to the tasks tree."
 
     (org-babel-do-load-languages
      'org-babel-load-languages '((python . t)
+                                 (calc . t)
                                  (emacs-lisp . t)
                                  (ruby . t)
                                  (clojure . t)
