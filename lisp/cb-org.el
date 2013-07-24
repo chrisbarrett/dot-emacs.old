5eedc9efb2094fb46f2de1198d3578f37e9ae0bb
Chris Barrett <chris.d.barrett@me.com>
2013-07-25 00:50:22 +1200

    Use M-o prefix for global org commands

--- lisp/cb-org.el
+++ lisp/cb-org.el
@@ -81,17 +81,17 @@ Make the 'q' key restore the previous window configuration."
                               (derived-mode-p 'org-agenda-mode))))
           (buffer-local-set-key (kbd "q") (command (restore))))))
 
-    (define-prefix-command 'cb:org-map)
+    ;; Override the default M-o bindings with org commands.
+    (global-unset-key (kbd "M-o"))
     (bind-keys
-      :global t
-      :map cb:org-map
-      "C-o a" (command (org-agenda-list nil nil 1))
-      "C-o d" (command (find-file org-agenda-diary-file))
-      "C-o c" (command (org-capture))
-      "C-o K" (command (org-capture nil "T"))
-      "C-o k" (command (org-capture nil "t"))
-      "C-o n" (command (find-file org-default-notes-file))
-      "C-o t" 'cb-org:show-todo-list)
+      :overriding? t
+      "M-o a" (command (org-agenda-list nil nil 1))
+      "M-o d" (command (find-file org-agenda-diary-file))
+      "M-o c" (command (org-capture))
+      "M-o K" (command (org-capture nil "T"))
+      "M-o k" (command (org-capture nil "t"))
+      "M-o n" (command (find-file org-default-notes-file))
+      "M-o t" 'cb-org:show-todo-list)
 
     (evil-define-keys 'normal org-mode-map
       "<return>" 'org-return
