d8733acc40d774f52c38c3987f3e89d1139ba51a
Chris Barrett <chris.d.barrett@me.com>
2013-08-09 21:20:25 +1200

    Set default value for org-global-properties

--- lisp/cb-org.el
+++ lisp/cb-org.el
@@ -522,6 +522,11 @@ With prefix argument ARG, show the file and move to the tasks tree."
           org-clock-persist-query-resume nil
           org-clock-report-include-clocking-task t)
 
+    (add-to-list 'org-global-properties
+                 `("Effort_ALL" .
+                   ,(concat "1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00 9:00 "
+                            "0:05 0:10 0:30")))
+
     ;; Remove empty LOGBOOK drawers when clocking out.
     (hook-fn 'org-clock-out-hook
       :append t
