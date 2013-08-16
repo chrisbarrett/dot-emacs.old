fb5f6f9be7efa502e036b582c2c917afe9198816
Chris Barrett <chris.d.barrett@me.com>
2013-08-16 16:16:48 +1200

    Extend day to 4am in agenda

--- lisp/cb-org.el
+++ lisp/cb-org.el
@@ -521,6 +521,8 @@ Non-nil if modifications where made."
             org-agenda-skip-deadline-if-done t
             org-agenda-skip-scheduled-if-done t
             org-agenda-skip-deadline-prewarning-if-scheduled t
+            ;; For all you night owls.
+            org-extend-today-until 4
             ;; Searches include archives
             org-agenda-text-search-extra-files '(agenda-archives)
             ;; Ensure the agenda shows the whole coming week.
