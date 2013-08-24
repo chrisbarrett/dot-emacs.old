0250d2999ca7ff570f6d639b6771d45c20bb1af6
Chris Barrett <chris.d.barrett@me.com>
2013-08-25 11:58:52 +1200

    Show growl notification when capturing with org-protocol

--- scripts/OrgCaptureChrome_Mac.rb
+++ scripts/OrgCaptureChrome_Mac.rb
@@ -5,9 +5,11 @@
 
 framework 'ScriptingBridge'
 require 'open-uri'
+require 'rubygems'
+require 'ruby_gntp'
 
 def escape(str)
-  URI.escape str, /[:?\/]/
+  URI.escape str, /[:?\/']/
 end
 
 # Get the URL and title of the current Chrome tab.
@@ -16,9 +18,17 @@ tab = chrome.windows[0].activeTab
 url = escape tab.URL
 title = escape tab.title
 # Get the current selected text.
+prev_pb = `pbpaste`
 tab.copySelection
-body = escape `pbpaste`
+body = escape `pbpaste` if `pbpaste` != prev_pb
 
 # Call Emacs Client to capture.
 template_key = 'l'
 `emacsclient org-protocol:/capture:/#{template_key}/'#{url}'/'#{title}'/'#{body}'`
+
+# Display Growl notification.
+GNTP.notify({
+  app_name: 'org-protocol',
+  title: 'Link Captured',
+  text: tab.title,
+})
