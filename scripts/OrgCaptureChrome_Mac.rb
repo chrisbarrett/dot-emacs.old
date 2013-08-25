898d1405ca103aa0a20452537c4f95c121643b4d
Chris Barrett <chris.d.barrett@me.com>
2013-08-25 12:18:04 +1200

    Update capture workflow, test for growl gem

--- scripts/OrgCaptureChrome_Mac.rb
+++ scripts/OrgCaptureChrome_Mac.rb
@@ -5,30 +5,29 @@
 
 framework 'ScriptingBridge'
 require 'open-uri'
-require 'rubygems'
-require 'ruby_gntp'
 
 def escape(str)
   URI.escape str, /[:?\/']/
 end
 
-# Get the URL and title of the current Chrome tab.
+# Get the current Chrome tab.
 chrome = SBApplication.applicationWithBundleIdentifier 'com.google.Chrome'
 tab = chrome.windows[0].activeTab
-url = escape tab.URL
-title = escape tab.title
-# Get the current selected text.
-prev_pb = `pbpaste`
-tab.copySelection
-body = escape `pbpaste` if `pbpaste` != prev_pb
-
-# Call Emacs Client to capture.
-template_key = 'l'
-`emacsclient org-protocol:/capture:/#{template_key}/'#{url}'/'#{title}'/'#{body}'`
 
 # Display Growl notification.
 GNTP.notify({
   app_name: 'org-protocol',
   title: 'Link Captured',
   text: tab.title,
-})
+}) if (require 'rubygems') && (require 'ruby_gntp')
+
+# Get fields to capture.
+url = escape tab.URL
+title = escape tab.title
+# Get the current selected text.
+prev_pb = `pbpaste`
+tab.copySelection
+body = escape `pbpaste` if `pbpaste` != prev_pb
+# Call Emacs Client to capture.
+template_key = 'l'
+`emacsclient org-protocol:/capture:/#{template_key}/'#{url}'/'#{title}'/'#{body}'`
