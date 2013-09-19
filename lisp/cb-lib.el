dc4c8064b22c4f4e2cd53fc5d0b116e090056093
Chris Barrett <chris.d.barrett@me.com>
2013-09-20 00:53:07 +1200

    Add shell command stuff to lib

--- lisp/cb-lib.el
+++ lisp/cb-lib.el
@@ -485,6 +485,78 @@ In batch mode, this just prints a summary instead of progress."
       (evil-append-line 1)
     (end-of-line)))
 
+;;; ----------------------------------------------------------------------------
+
+(defvar %-sudo-liftable-commands '(%
+                                   %-async
+                                   %-str
+                                   shell-command
+                                   async-shell-command
+                                   shell-command-to-string)
+  "A list of commands that may be escalated using the `%-sudo' macro.")
+
+
+(defun % (command &rest arguments)
+  "Run COMMAND with ARGUMENTS, returning the exit code."
+  (shell-command (apply 'concat command (s-join " " arguments))))
+
+(defun %-str (command &rest arguments)
+  "Run COMMAND with ARGUMENTS, returning its output as a string."
+  (shell-command-to-string (apply 'concat command (s-join " " arguments))))
+
+(defun %-async (command &rest arguments)
+  "Run COMMAND with ARGUMENTS, returning its output as a string."
+  (async-shell-command (apply 'concat command (s-join " " arguments))))
+
+(defun %-can-sudo-without-passwd? ()
+  "Test whether we are currently able to sudo without entering a password."
+  (zerop (shell-command "sudo -n true")))
+
+(defmacro %-sudo (command)
+  "Execute a shell command with escalated privileges.
+
+COMMAND must be a direct call to one of the forms listed in
+`sudo-liftable-commands'.
+
+The sudo command will likely be configured with a timeout on your
+system.  The user will be interactively prompted for their
+password if necessary.  Subsequent calls to sudo within the
+timeout period will not require the password again."
+  (cl-assert command)
+  (cl-assert (listp command))
+  (cl-assert (-contains? %-sudo-liftable-commands (car command)))
+
+  ;; Reach into the command and replace the direct shell command argument,
+  ;; wrapping it with a call to sudo.
+  ;;
+  ;; There are two execution paths, depending on whether the user is currently
+  ;; authenticated with sudo.
+  (cl-destructuring-bind (fn cmd &rest args) command
+    (let ((g-passwd (cl-gensym))
+          (g-result (cl-gensym)))
+      `(-if-let (,g-passwd (unless (%-can-sudo-without-passwd?) (%-read-passwd)))
+
+           ;; Path 1. The password is required: Consume the password and
+           ;; tidy the shell output. Finally, delete the password string from
+           ;; memory.
+           (unwind-protect
+               (let ((,g-result
+                      (,fn
+                       (format "echo %s | sudo -S %s"
+                               (shell-quote-argument ,g-passwd) ,cmd)
+                       ,@args)))
+                 ;; Annoyingly, the password prompt gets prepended to string
+                 ;; output and must be stripped.
+                 (if (stringp ,g-result)
+                     (s-chop-prefix "Password:" ,g-result)
+                   ,g-result))
+             ;; Clear the password from memory.
+             (clear-string ,g-passwd))
+
+         ;; Path 2. We are within the sudo timeout period: The password is not
+         ;; required and we can call the command with sudo prefixed.
+         (,fn (format "sudo %s" ,cmd) ,@args)))))
+
 (provide 'cb-lib)
 
 ;; Local Variables:
