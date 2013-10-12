04e339caf013bbf29252e3956aee0bcff35688bb
Chris Barrett <chris.d.barrett@me.com>
2013-10-13 01:41:09 +1300

    todo capture template schedules task for today

--- lisp/cb-org.el
+++ lisp/cb-org.el
@@ -551,6 +551,7 @@ the date TARGET-DAY, TARGET-MONTH each year."
                  (file+headline org-default-notes-file "Tasks")
                  ,(s-unlines
                    "* TODO %?"
+                   "SCHEDULED: %u"
                    ":PROPERTIES:"
                    ":CAPTURED: %U"
                    ":END:")
