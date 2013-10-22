61abf6a7f23c0e2c790709b96231718476bdb9b7
Chris Barrett <chris.d.barrett@me.com>
2013-10-23 11:08:06 +1300

    Add vim-like spelling error commands

--- lisp/cb-evil.el
+++ lisp/cb-evil.el
@@ -39,6 +39,72 @@ Each handler should take the search string as an argument.")
 (configuration-group
   :when (true? cb:use-vim-keybindings?)
 
+  (defun cbevil:error-backward-search-start-pos (pos)
+    "Wrap the search to the beginning of the buffer if there are no
+errors forward of POS."
+    (if (and (eq (current-buffer) flyspell-old-buffer-error)
+             (eq pos flyspell-old-pos-error))
+        (cond
+         ((= flyspell-old-pos-error (point-min))
+          (message "Restarting from end of buffer")
+          (point-max))
+         (t
+          (save-excursion
+            (forward-word -1)
+            (point))))
+      (point)))
+
+  (defun cbevil:prev-spelling-error-pos ()
+    (let ((pos (cbevil:error-backward-search-start-pos (point))))
+      (while (and (> pos (point-min))
+                  (-none? 'flyspell-overlay-p (overlays-at pos)))
+        (cl-decf pos))
+      pos))
+
+  (defun evil-previous-spelling-error ()
+    "Go to the previous flyspell error."
+    (interactive)
+    (let ((pos (cbevil:prev-spelling-error-pos)))
+      ;; save the current location for next invocation
+      (setq flyspell-old-pos-error pos)
+      (setq flyspell-old-buffer-error (current-buffer))
+      (goto-char pos)
+      (when (= pos (point-min))
+        (message "No more spelling errors"))))
+
+  (defun cbevil:error-forward-search-start-pos (pos)
+    "Wrap the search to the beginning of the buffer if there are no
+errors forward of POS."
+    (if (and (eq (current-buffer) flyspell-old-buffer-error)
+             (eq pos flyspell-old-pos-error))
+        (cond
+         ((= flyspell-old-pos-error (point-max))
+          (message "Restarting from beginning of buffer")
+          (point-min))
+         (t
+          (save-excursion
+            (forward-word 1)
+            (point))))
+      (point)))
+
+  (defun cbevil:next-spelling-error-pos ()
+    (let ((pos (cbevil:error-forward-search-start-pos (point))))
+      (while (and (< pos (point-max))
+                  (-none? 'flyspell-overlay-p (overlays-at pos)))
+        (cl-incf pos))
+      pos))
+
+  (defun evil-next-spelling-error ()
+    "Go to the next flyspell error."
+    (interactive)
+    (let ((pos (cbevil:next-spelling-error-pos)))
+      ;; save the current location for next invocation
+      (setq flyspell-old-pos-error pos)
+      (setq flyspell-old-buffer-error (current-buffer))
+      (goto-char pos)
+      (when (= pos (point-max))
+        (message "No more spelling errors"))))
+
   (defun get-manpage (candidate)
     "Show the manpage for CANDIDATE."
     (let ((wfiles (mapcar 'car (woman-file-name-all-completions candidate))))
@@ -90,7 +156,9 @@ Creates STATE bindings for DEFS. DEFS are comprised of alternating string-symbol
         "SPC" 'evil-toggle-fold
         "K"   'cbevil:get-documentation
         "u"   'undo-tree-undo
-        "C-R" 'undo-tree-redo)
+        "C-R" 'undo-tree-redo
+        "[s"  'evil-next-spelling-error
+        "]s"  'evil-previous-spelling-error)
 
       (define-key evil-insert-state-map (kbd "C-z") 'evil-undefine)
       (define-key evil-emacs-state-map  (kbd "M-z") 'evil-normal-state)
