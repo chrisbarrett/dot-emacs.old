3baa029bc9dc2b4904d9417b6c246fec19bd2791
Chris Barrett <chris.d.barrett@me.com>
2013-11-02 11:07:26 +1300

    Add key command for magit blame mode

--- lisp/cb-git.el
+++ lisp/cb-git.el
@@ -72,6 +72,7 @@
       "g l" 'magit-log
       "g r" 'magit-reflog
       "g D" 'magit-diff-working-tree
+      "g B" 'magit-blame-mode
       "g b" (command
              (with-window-restore
                (magit-branch-manager)
