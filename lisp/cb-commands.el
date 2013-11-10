53d362248316a16367f00fb5f45c271408b6e3e5
Chris Barrett <chris.d.barrett@me.com>
2013-11-10 15:53:44 +1300

    Add minibuffer-macro-query

--- lisp/cb-commands.el
+++ lisp/cb-commands.el
@@ -274,6 +274,18 @@ PARAGRAPH-LENGTH is one of short, medium, long or verylong."
     (goto-char (line-beginning-position))
     (delete-horizontal-space)))
 
+;; Keyboard macros
+
+(defun minibuffer-macro-query (arg)
+  "Prompt for input using minibuffer during kbd macro execution.
+With prefix ARG, allows you to select what prompt string to use.
+If the input is non-empty, it is inserted at point."
+  (interactive "P")
+  (let* ((prompt (if arg (read-from-minibuffer "PROMPT: ") "Input: "))
+         (input (minibuffer-with-setup-hook (lambda () (kbd-macro-query t))
+                  (read-from-minibuffer prompt))))
+    (unless (string= "" input) (insert input))))
+
 (provide 'cb-commands)
 
 ;;; cb-commands.el ends here
