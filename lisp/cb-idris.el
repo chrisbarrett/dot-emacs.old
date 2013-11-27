192230ef620d73d8bdcb0429f2727a0f3dfb803a
Chris Barrett <chris.d.barrett@me.com>
2013-11-28 01:39:49 +1300

    Configure smart operators for idris

--- lisp/cb-idris.el
+++ lisp/cb-idris.el
@@ -28,6 +28,118 @@
 
 (require 'use-package)
 (require 'cb-lib)
+(require 'cb-mode-groups)
+
+;; Configure smart operators for Idris
+;;
+;; These are more or less duplicates of the Haskell functions.
+
+(defun cbidris:smart-comma ()
+  (interactive)
+  (cond
+   ((s-matches? (rx bol (* space) eol)
+                (buffer-substring (line-beginning-position) (point)))
+    (insert ", ")
+    (idris-indentation-indent-line))
+   (t
+    (insert ","))))
+
+(defun cbidris:smart-pipe ()
+  "Insert a pipe operator. Add padding, unless we're inside a list."
+  (interactive)
+  (if (s-matches? (rx "[" (* (any "|" alnum)) eol)
+                  (buffer-substring (line-beginning-position) (point)))
+      (insert "|")
+    (smart-insert-operator "|")))
+
+(defun cbidris:looking-at-module-or-constructor? ()
+  (-when-let (sym (thing-at-point 'symbol))
+    (s-uppercase? (substring sym 0 1))))
+
+(defun cbidris:smart-dot (&optional arg)
+  "Insert a period. Add padding, unless this line is an import statement.
+With a prefix arg, insert a period without padding."
+  (interactive "*P")
+  (cond
+   (arg
+    (insert "."))
+   ((cbidris:looking-at-module-or-constructor?)
+    (insert "."))
+   ((thing-at-point-looking-at (rx (or "(" "{" "[") (* space)))
+    (insert "."))
+   (t
+    (smart-insert-operator "."))))
+
+(defun cbidris:smart-colon ()
+  "Insert either a type binding colon pair or a cons colon."
+  (interactive)
+  (if (s-matches? (rx bol (* space) (? ",") (* space)
+                      (+ (not (any space "("))) (* space) eol)
+                  (buffer-substring (line-beginning-position) (point)))
+      (atomic-change-group
+        (just-one-space)
+        (insert "::")
+        (just-one-space))
+    (insert ":")))
+
+(defun cbidris:insert-arrow (arrow)
+  "If point is inside a tuple, insert an arrow inside.
+Otherwise insert an arrow at the end of the line."
+  (atomic-change-group
+    (cl-destructuring-bind (&key beg end op &allow-other-keys)
+        (sp-get-sexp t)
+      ;; Check whether point is inside a tuple.
+      (if (and (equal op "(")
+               (> (point) beg)
+               (< (point) end))
+          (sp-end-of-sexp)
+        (end-of-line)))
+    ;; Insert arrow.
+    (just-one-space)
+    (insert arrow)
+    (just-one-space)))
+
+(defun cbidris:at-typedecl? ()
+  (s-matches? (rx bow ":" eow) (buffer-substring (line-beginning-position) (point))))
+
+(defun cbidris:smart-minus (&optional arg)
+  "Insert an arrow if we're in a typesig, otherwise perform a normal insertion.
+With a prefix arg, insert an arrow with padding at point."
+  (interactive "*P")
+  (cond
+   (arg
+    (just-one-space)
+    (insert "->")
+    (just-one-space))
+   ((cbidris:at-typedecl?)
+    (cbidris:insert-arrow "->"))
+   (t
+    (smart-insert-operator "-"))))
+
+(defun cbidris:smart-lt (&optional arg)
+  "Insert a less than symbol. With a prefix arg, insert an arrow at point."
+  (interactive "*P")
+  (cond
+   (arg
+    (just-one-space)
+    (insert "<-")
+    (just-one-space))
+   (t
+    (smart-insert-operator "<"))))
+
+(bind-keys
+  :hook cb:idris-modes-hook
+  "," 'cbidris:smart-comma
+  "-" 'cbidris:smart-minus
+  "=" (command (smart-insert-operator "="))
+  "<" 'cbidris:smart-lt
+  "." 'cbidris:smart-dot
+  ":" 'cbidris:smart-colon
+  "|" 'cbidris:smart-pipe
+  "?" (command (smart-insert-operator "?"))
+  "$" (command (smart-insert-operator "$")))
+
+(add-hook 'cb:idris-modes-hook 'smart-insert-operator-hook)
 
 ;; `idris-mode' provides editing support for the Idris language.
 (use-package idris-mode
