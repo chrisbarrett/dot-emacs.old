b77d0557281496b4ac642d05cff44a0344dfae2a
Chris Barrett <chris.d.barrett@me.com>
2013-11-27 14:11:54 +1300

    Add idris-mode

---
+++ lisp/cb-idris.el
@@ -0,0 +1,37 @@
+;;; cb-idris.el --- Configuration for the Idris language.
+
+;; Copyright (C) 2013 Chris Barrett
+
+;; Author: Chris Barrett <chris.d.barrett@me.com>
+;; Version: 0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Configuration for the Idris language.
+
+;;; Code:
+
+(require 'use-package)
+(require 'cb-lib)
+
+(use-package idris-mode
+  :mode (("\\.idr$" . idris-mode)))
+
+(provide 'cb-idris)
+
+;;; cb-idris.el ends here
