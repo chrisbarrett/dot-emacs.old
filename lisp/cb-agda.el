e16e26e7dad6d94da5bb8658e35c4cb3b58c603c
Chris Barrett <chris.d.barrett@me.com>
2013-11-27 13:33:15 +1300

    Add agda mode

---
+++ lisp/cb-agda.el
@@ -0,0 +1,40 @@
+;;; cb-agda.el --- Configuration for the Agda programming language.
+
+;; Copyright (C) 2013 Chris Barrett
+
+;; Author: Chris Barrett <chris.d.barrett@me.com>
+;; Version: 0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Configuration for the Agda programming language.
+
+;;; Code:
+
+(require 'cb-lib)
+(require 'use-package)
+
+(use-package agda2)
+
+;; Enable auto-complete in Agda buffers.
+(after 'auto-complete
+  (add-to-list 'ac-modes 'agda-mode))
+
+(provide 'cb-agda)
+
+;;; cb-agda.el ends here
