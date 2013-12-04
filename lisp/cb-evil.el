d30787999655260c72f578e648ddcbf46b561053
Chris Barrett <chris.d.barrett@me.com>
2013-12-05 01:37:08 +1300

    Enter insertion state after auto-insert.

--- lisp/cb-evil.el
+++ lisp/cb-evil.el
@@ -131,6 +131,11 @@ errors forward of POS."
     (when (= pos (point-max))
       (message "No more spelling errors"))))
 
+;; Enter insertion state after auto-insert.
+(after 'autoinsert
+  (defadvice auto-insert (after evil-insert activate)
+    (evil-insert-state)))
+
 ;;; Mode-appropriate documentation search with K.
 
 (defun get-manpage (candidate)
