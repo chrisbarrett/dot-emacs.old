e4b31f2621ddd064c91100bdd4ccd9ce66d22dd1
Chris Barrett <chris.d.barrett@me.com>
2013-12-06 12:10:26 +1300

    Add offset to yasnippet nav in ledger mode

--- lisp/cb-ledger.el
+++ lisp/cb-ledger.el
@@ -29,6 +29,12 @@
 (require 'cb-lib)
 (require 'use-package)
 
+(after 'cb-yasnippet
+  (defadvice cbyas:end-of-field (around offset-for-ledger-face-weirdness activate)
+    "Ledger-mode reports incorrect field ends. Add an offset to correct this."
+    (-when-let (pos ad-do-it)
+      (1+ pos))))
+
 ;; `ledger-mode' provides support for ledger files.
 (use-package ledger-mode
   :ensure t
