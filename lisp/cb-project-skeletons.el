e1f306305641fabc11329f3dcf255d9a000b1a8a
Chris Barrett <chris.d.barrett@me.com>
2013-12-16 22:31:12 +1300

    Rework skeletons to use declarative macro

--- lisp/cb-project-skeletons.el
+++ lisp/cb-project-skeletons.el
@@ -42,7 +42,23 @@
   "The directory containing license files for projects.")
 
 (defvar skel-project-folder (f-join user-home-directory "Projects")
-  "The directory where projects should be created.")
+  "The directory where projects will be created.")
+
+(defvar skel-default-replacements
+  (list (cons "__YEAR__" (format-time-string "%Y"))
+        (cons "__USER__" user-full-name)
+        (cons "__ORGANISATION__" (or (true? user-organisation) user-full-name)))
+  "A list of replacements available for expansion in project skeletons.")
+
+(defvar skel-init-with-git t
+  "When non-nil, initialise newly created projects with a git repository.")
+
+(defvar skel-after-project-instantiated-hook nil
+  "Hook run after a project is successfully instantiated.
+Each function will be passed the path of the newly instantiated
+project.")
+
+;;;;;;;;;;;;;;;;;;;;;;;; Internal ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun skel:join-patterns (patterns)
   "Turn PATTERNS into a string that `find' can use."
@@ -75,74 +91,34 @@
     (--each replacements
       (goto-char 0)
       (while (search-forward (car it) nil t)
-        (replace-match (cdr it))))))
+        (replace-match (cdr it) 'fixcase 'literal)))))
 
-(defun skel:instantiate-template-directory (template folder replacements)
-  "Create the directory for TEMPLATE at destination FOLDER.
+(defun skel:instantiate-template-directory (template dest replacements)
+  "Create the directory for TEMPLATE at destination DEST.
 Performs the substitutions specified by REPLACEMENTS."
-  (let ((tmp-folder (f-expand (concat "__skeleton_tmp_" template) skel-project-folder)))
-    (when (f-exists? tmp-folder) (f-delete tmp-folder t))
-    (copy-directory (f-expand template skel-directory) tmp-folder nil nil t)
-    (--each (skel:files-matching (--map (s-concat "*" (car it) "*") replacements) tmp-folder)
-      (rename-file it (s-replace-all replacements it)))
-    (--each (skel:files-matching ["*"] tmp-folder "-type f")
-      (skel:instantiate-template-file it replacements))
-    (copy-directory tmp-folder folder)
-    (delete-directory tmp-folder t)))
+  (let ((tmpd (make-temp-file "project-skeleton__" t)))
+    (unwind-protect
 
-(defun skel:instantiate-license-file (license-file destination replacements)
+        (progn
+          (--each (f-entries (f-expand template skel-directory))
+            (f-copy it tmpd))
+
+          ;; Process file name and contents according to replacement rules.
+          (--each (f-entries tmpd nil t)
+            (let ((updated (s-replace-all replacements it)))
+              (unless (equal updated it)
+                (rename-file it updated))))
+
+          (--each (f-files tmpd nil t)
+            (skel:instantiate-template-file it replacements))
+
+          (copy-directory tmpd dest)))
+
+    (delete-directory tmpd t)))
+
+(defun skel:instantiate-license-file (license-file dest replacements)
   "Populate the given license file template."
-  (let ((repls (cl-list* (cons "__year__" (format-time-string "%Y"))
-                         (cons "__user-name__" user-full-name)
-                         (cons "__organisation__" (or (true? user-organisation) user-full-name))
-                         replacements)))
-
-    (f-write (s-replace-all repls (f-read license-file))
-             'utf-8
-             destination)))
-
-(defmacro with-new-project (project-name skeleton license replacements &rest body)
-  "Declare a new project.
-
-* PROJECT-NAME is a string naming the project.
-
-* SKELETON is a string describing the project type.
-
-* LICENSE is the path to the license file to use as a template.
-
-* REPLACEMENTS is an alist of (string . replacement) used specify
-  substitutions when initialising the project from its skeleton.
-
-* BODY forms are executed at the project root once the project is created."
-  (declare (indent 2))
-  `(let ((folder (f-expand ,project-name skel-project-folder)))
-
-     (message "Initialising project...")
-
-     (skel:instantiate-template-directory ,skeleton folder (list ,@replacements))
-     (skel:instantiate-license-file ,license (f-join folder "LICENSE") (list ,@replacements))
-     (let ((default-directory (f-slash folder)))
-       ,@body
-       (%-async "git init && git add -A && git commit -m 'Initial commit'"))
-
-     (message "Initialising project...done")))
-
-(defun declare-project-skeleton (name fn)
-  "Add project skeleton to the list of available ones.
-
-* NAME is the name of the project.
-
-* FN is an interactive command that will be called to initialise
-  the project."
-  (add-to-list 'skel-project-skeletons (cons name fn)))
-
-(defun create-project (type)
-  "Create a project of the given TYPE."
-  (interactive
-   (list (completing-read "Skeleton: " (-map 'car skel-project-skeletons) nil t)))
-  (call-interactively (cdr (assoc type skel-project-skeletons))))
-
-;; License file creation.
+  (f-write (s-replace-all replacements (f-read license-file)) 'utf-8 dest))
 
 (defun skel-read-license (prompt default)
   "Prompt the user to select a license.
@@ -156,10 +132,117 @@ Performs the substitutions specified by REPLACEMENTS."
          (choice (ido-completing-read prompt (-map 'car xs) nil t d)))
     (cdr (assoc choice xs))))
 
-;; Load skeletons.
-(when (f-exists? skel-directory)
-  (--each (directory-files skel-directory nil "^[^#].*el$")
-    (load (f-expand it skel-directory))))
+(defun skel:initialize-git-repo  (dir)
+  "Initialise a new git repository at DIR."
+  (when skel-init-with-git
+    (message "Initialising git...")
+    (shell-command
+     (format "cd %s && git init && git add -A && git commit -m 'Initial commit'"
+             (shell-quote-argument dir)))
+    (message "Initialising git...done")))
+
+;;;;;;;;;;;;;;;;;;;;;;;; User commands ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;###autoload
+(cl-defmacro define-project-skeleton
+    (name &key replacements after-creation default-license)
+  "Declare a new project type
+
+* NAME is a string naming the project type.
+
+* REPLACEMENTS is an alist of (string . replacement) used specify
+  substitutions when initialising the project from its skeleton.
+
+* DEFAULT-LICENSE is a regexp matching the name of a license to
+  be used as the default when reading from the user.
+
+* AFTER-CREATION is a unary function to be run once the project
+  is created. It should take a single argument--the path to the
+  newly-created project."
+  (declare (indent 1))
+
+  (let ((constructor (intern (format "create-%s" name)))
+        (default-license-var (intern (format "%s-default-license" name))))
+    `(progn
+
+       (defvar ,default-license-var ,default-license
+         ,(concat "Auto-generated variable.\n\n"
+                  "The default license type for " name " skeletons.") )
+
+       (defun ,constructor (project-name license-file)
+
+         ,(concat
+           "Auto-generated function.\n\n"
+           "Interactively creates a new " name " skeleton.\n"
+           "
+* PROJECT-NAME is the name of this project instance.
+
+* LICENSE-FILE is the path to a license file to be added to the project.")
+
+         (interactive (list (read-string "Project name: ")
+                            (skel-read-license "License: " ,default-license-var)))
+
+         (let* ((dest (f-join skel-project-folder project-name))
+                (default-directory dest)
+                (repls (-concat '(("__PROJECT-NAME__" . ,name))
+                                skel-default-replacements
+                                ,replacements)))
+
+           (skel:instantiate-template-directory ,name dest repls)
+           (skel:instantiate-license-file license-file (f-join dest "LICENSE") repls)
+           (funcall ,after-creation dest)
+           (skel:initialize-git-repo dest)
+           (run-hook-with-args 'skel-after-project-instantiated-hook default-directory)))
+
+       (add-to-list 'skel-project-skeletons (cons ,name ',constructor)))))
+
+(add-hook 'skel-after-project-instantiated-hook (lambda (dir) (dired dir)))
+
+;;;###autoload
+(defun create-project (type)
+  "Create a project of the given TYPE."
+  (interactive (list (completing-read "Skeleton: "
+                                      (-sort 'string< (-map 'car skel-project-skeletons))
+                                      nil t)))
+  (let ((constructor (cdr (assoc type skel-project-skeletons))))
+    (call-interactively constructor)))
+
+;;;;;;;;;;;;;;;;;;;;;;;; Define skeletons ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-project-skeleton "elisp-package"
+  :default-license "^gpl"
+  :after-creation
+  (lambda (dir)
+    (%-async (format "cd %s && cask" (%-quote dir)))))
+
+(defun cbpy:python-binaries ()
+  "Find python binaries in bin directories."
+  (--mapcat
+   (f-files it (~ s-matches? (rx "python" (* (any digit "." "-")) eol)))
+   '("/usr/bin" "/usr/local/bin")))
+
+(defun cbpy:create-virtualenv-dirlocals (dir)
+  "Create a .dir-locals file for virtualenv variables."
+  (save-excursion
+    (add-dir-local-variable nil 'virtualenv-default-directory dir)
+    (add-dir-local-variable nil 'virtualenv-workon (f-filename dir))
+    (save-buffer)
+    (kill-buffer)))
+
+(defun cbpy:init-virtualenv (dir)
+  "Initialise a virtualenv environment at DIR."
+  (let ((python-bin (ido-completing-read "Python binary: " (cbpy:python-binaries)))
+        (dest (f-join dir "env")))
+    (unless (zerop (%-sh "virtualenv" "-p" (%-quote python-bin) dest))
+      (error "Virtualenv failed"))))
+
+(define-project-skeleton "python-project"
+  :default-license "^bsd"
+  :after-creation
+  (lambda (dir)
+    (cbpy:init-virtualenv dir)
+    (let ((inhibit-redisplay t))
+      (cbpy:create-virtualenv-dirlocals dir))))
 
 (provide 'cb-project-skeletons)
 
--- project-skeletons/elisp-package/features/support/env.el
+++ project-skeletons/elisp-package/features/support/env.el
@@ -1,9 +1,9 @@
 (let* ((current-directory (file-name-directory load-file-name))
        (features-directory (expand-file-name ".." current-directory))
        (project-directory (expand-file-name ".." features-directory)))
-  (setq __project-name__-root-path project-directory))
+  (setq __PROJECT-NAME__-root-path project-directory))
 
-(add-to-list 'load-path __project-name__-root-path)
+(add-to-list 'load-path __PROJECT-NAME__-root-path)
 
 (require '__project-name__)
 (require 'espuds)
@@ -11,7 +11,7 @@
 
 (Before
  (switch-to-buffer
-  (get-buffer-create "*__project-name__*"))
+  (get-buffer-create "*__PROJECT-NAME__*"))
  (erase-buffer)
  (fundamental-mode)
  (deactivate-mark))
