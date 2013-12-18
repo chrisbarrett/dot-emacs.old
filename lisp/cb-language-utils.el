f1d0dc6b7dcdb15ecfd518187ac4d4d64ac9cae9
Chris Barrett <chris.d.barrett@me.com>
2013-12-19 10:55:36 +1300

    Rebind emr key

--- lisp/cb-language-utils.el
+++ lisp/cb-language-utils.el
@@ -31,7 +31,7 @@
 
 (use-package emr
   :ensure t
-  :bind ("M-RET" . emr-show-refactor-menu)
+  :bind ("M-S-<return>" . emr-show-refactor-menu)
   :init (add-hook 'prog-mode-hook 'emr-initialize))
 
 (use-package smart-operator
