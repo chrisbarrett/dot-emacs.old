3b843e5c206dcacdb75f25c3854bcce5a95156d2
Chris Barrett <chris.d.barrett@me.com>
2014-07-28 00:49:14 +1200

    Clean up clojure config

--- lisp/config-clojure.el
+++ lisp/config-clojure.el
@@ -39,8 +39,7 @@
  '(cider-popup-stacktraces nil)
  '(nrepl-hide-special-buffers t))
 
-(after 'evil
-  (define-evil-doc-handler cb:clojure-modes (call-interactively 'cider-doc)))
+;;; Cider configuration
 
 (defun cb:switch-to-cider ()
   "Start cider or switch to an existing cider buffer."
@@ -49,89 +48,72 @@
       (cider-switch-to-repl-buffer buf)
     (cider-jack-in)))
 
-(after 'clojure-mode
-  (define-key clojure-mode-map (kbd "C-c C-z") 'cb:switch-to-cider))
+(defadvice cider-popup-buffer-display (after set-mode activate)
+  (with-current-buffer (ad-get-arg 0)
+    (help-mode)))
+
+(defun cb:switch-to-clojure ()
+  "Switch to the last active clojure buffer."
+  (interactive)
+  (-when-let (buf (--first-buffer (derived-mode-p 'clojure-mode)))
+    (pop-to-buffer buf)))
+
+(defun cb:eval-last-clj-buffer ()
+  "Evaluate that last active clojure buffer without leaving the repl."
+  (interactive)
+  (-when-let (buf (--first-buffer (derived-mode-p 'clojure-mode)))
+    (with-current-buffer buf
+      (cider-eval-buffer))))
+
+(defadvice cider-switch-to-repl-buffer (after insert-at-end-of-cider-line activate)
+  (cb:maybe-evil-insert-state))
+
+(defadvice back-to-indentation (around move-to-cider-bol activate)
+  "Move to position after prompt in cider."
+  (if (equal major-mode 'cider-mode)
+      (nrepl-bol)
+    ad-do-it))
+
+(add-hook 'cb:clojure-modes-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-repl-mode-hook 'cb:maybe-evil-insert-state)
 
 (after 'cider
-
-  (define-key clojure-mode-map (kbd "C-c C-h") 'cider-doc)
-
-  (defadvice cider-popup-buffer-display (after set-mode activate)
-    (with-current-buffer (ad-get-arg 0)
-      (help-mode)))
-
-  (defun cb:switch-to-clojure ()
-    "Switch to the last active clojure buffer."
-    (interactive)
-    (-when-let (buf (--first-buffer (derived-mode-p 'clojure-mode)))
-      (pop-to-buffer buf)))
-
-  (define-key cider-repl-mode-map (kbd "C-c C-z") 'cb:switch-to-clojure)
-
-  (after 'cider-interaction
-
-    (defun cider-emit-doc-into-popup-buffer (buffer value)
-      "Emit into BUFFER the provided VALUE."
-      (with-current-buffer buffer
-        (let ((inhibit-read-only t)
-              (buffer-undo-list t))
-          (goto-char (point-max))
-          (insert (format "%s" value))
-          (indent-sexp)
-          (font-lock-fontify-buffer)
-          (goto-char (point-min)))))
-
-    (defun cider-doc--handler (buffer)
-      "Make a handler for evaluating and printing stdout/stderr in popup BUFFER."
-      (nrepl-make-response-handler buffer
-                                   '()
-                                   (lambda (buffer str)
-                                     (cider-emit-doc-into-popup-buffer buffer str))
-                                   (lambda (buffer str)
-                                     (cider-emit-doc-into-popup-buffer buffer str))
-                                   '()))
-
-    (defun cider-doc-handler (symbol)
-      "Create a handler to lookup documentation for SYMBOL."
-      (let ((form (format "(clojure.repl/doc %s)" symbol))
-            (doc-buffer (cider-popup-buffer cider-doc-buffer t)))
-        (cider-tooling-eval form
-                            (cider-doc--handler doc-buffer)
-                            nrepl-buffer-ns))))
-
-  (hook-fns '(clojure-mode-hook cider-repl-mode-hook)
-    (cider-turn-on-eldoc-mode))
-
-  (-each (--filter-buffers (derived-mode-p 'clojure-mode))
-    'cider-turn-on-eldoc-mode)
-
-  (defun cb:eval-last-clj-buffer ()
-    "Evaluate that last active clojure buffer without leaving the repl."
-    (interactive)
-    (-when-let (buf (--first-buffer (derived-mode-p 'clojure-mode)))
-      (with-current-buffer buf
-        (cider-eval-buffer))))
-
-  (define-key clojure-mode-map (kbd "C-c C-f") 'cider-eval-buffer)
-  (define-key cider-repl-mode-map (kbd "C-c C-f") 'cb:eval-last-clj-buffer)
-
   (set-face-attribute 'cider-error-highlight-face t :inherit 'error)
-  (set-face-underline 'cider-error-highlight-face nil)
+  (set-face-underline 'cider-error-highlight-face nil))
 
-  (add-hook 'cider-repl-mode-hook 'cb:maybe-evil-insert-state)
+;;; Documentation search
 
-  (defadvice cider-switch-to-repl-buffer (after insert-at-end-of-cider-line activate)
-    (cb:maybe-evil-insert-state))
+(defun cider-emit-doc-into-popup-buffer (buffer value)
+  "Emit into BUFFER the provided VALUE."
+  (with-current-buffer buffer
+    (let ((inhibit-read-only t)
+          (buffer-undo-list t))
+      (goto-char (point-max))
+      (insert (format "%s" value))
+      (indent-sexp)
+      (font-lock-fontify-buffer)
+      (goto-char (point-min)))))
 
-  (defadvice back-to-indentation (around move-to-cider-bol activate)
-    "Move to position after prompt in cider."
-    (if (equal major-mode 'cider-mode)
-        (nrepl-bol)
-      ad-do-it))
+(defun cider-doc--handler (buffer)
+  "Make a handler for evaluating and printing stdout/stderr in popup BUFFER."
+  (nrepl-make-response-handler buffer
+                               '()
+                               (lambda (buffer str)
+                                 (cider-emit-doc-into-popup-buffer buffer str))
+                               (lambda (buffer str)
+                                 (cider-emit-doc-into-popup-buffer buffer str))
+                               '()))
 
-  (define-key cider-repl-mode-map (kbd "C-l") 'cider-repl-clear-buffer)
+(after 'cider
+  (defun cider-doc-handler (symbol)
+    "Create a handler to lookup documentation for SYMBOL."
+    (let ((form (format "(clojure.repl/doc %s)" symbol))
+          (doc-buffer (cider-popup-buffer cider-doc-buffer t)))
+      (cider-tooling-eval form
+                          (cider-doc--handler doc-buffer)
+                          nrepl-buffer-ns))))
 
-  )
+;;; Snippet utilities
 
 (defun cbclj:pad-for-arglist (text)
   "Pad TEXT for insertion into an arglist after existing parameters."
@@ -150,58 +132,17 @@
                    (f-no-ext (f-filename (buffer-file-name)))))
     "name"))
 
-(defun cb:stop-overtone ()
-  "Stop synthesis."
-  (interactive)
-  (cider-eval "(stop)" nil)
-  (message "Synthesis stopped."))
+;;; Key bindings
 
-(defun overtone-doc-handler (symbol)
-  "Create a handler to lookup documentation for SYMBOL."
-  (let ((form (format "(odoc %s)" symbol))
-        (doc-buffer (cider-popup-buffer cider-doc-buffer t)))
-    (cider-tooling-eval form
-                        (cider-popup-eval-out-handler doc-buffer)
-                        nrepl-buffer-ns)))
+(after 'clojure-mode
+  (define-key clojure-mode-map (kbd "C-c C-z") 'cb:switch-to-cider)
+  (define-key clojure-mode-map (kbd "C-c C-h") 'cider-doc)
+  (define-key clojure-mode-map (kbd "C-c C-f") 'cider-eval-buffer))
 
-(defun overtone-doc (query)
-  "Open a window with the docstring for the given QUERY.
-Defaults to the symbol at point.  With prefix arg or no symbol
-under point, prompts for a var."
-  (interactive "P")
-  (cider-read-symbol-name "Symbol: " 'overtone-doc-handler query))
-
-(defalias 'odoc 'overtone-doc)
-
-(defun cbot:overtone-project-reference-p ()
-  "Non-nil if the project.clj imports overtone."
-  (-when-let (clj (and (projectile-project-p)
-                       (f-join (projectile-project-root) "project.clj")))
-    (when (f-exists? clj)
-      (s-contains? "overtone" (f-read-text clj)))))
-
-(defvar overtone-mode-map
-  (let ((km (make-sparse-keymap)))
-    (define-key km (kbd "C-c C-g") 'cb:stop-overtone)
-    (define-key km (kbd "s-.") 'cb:stop-overtone)
-    (define-key km (kbd "C-c C-h") 'odoc)
-    km))
-
-(define-minor-mode overtone-mode
-  "Provide additional overtone-related functionality for clojure."
-  nil " overtone" overtone-mode-map)
-
-(defun maybe-enable-overtone-mode ()
-  "Enable `overtone-mode' only if the current buffer or project references overtone."
-  (when (and (not overtone-mode)
-             (derived-mode-p 'clojure-mode 'cider-repl-mode)
-             (cbot:overtone-project-reference-p))
-    (overtone-mode t)))
-
-(define-globalized-minor-mode global-overtone-mode overtone-mode
-  maybe-enable-overtone-mode)
-
-(add-hook 'clojure-mode-hook 'global-overtone-mode)
+(after 'cider
+  (define-key cider-repl-mode-map (kbd "C-c C-z") 'cb:switch-to-clojure)
+  (define-key cider-repl-mode-map (kbd "C-c C-f") 'cb:eval-last-clj-buffer)
+  (define-key cider-repl-mode-map (kbd "C-l") 'cider-repl-clear-buffer))
 
 (provide 'config-clojure)
 
