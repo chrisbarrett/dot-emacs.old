41a82c91ecb67afc5050552a20baac92264452b7
Chris Barrett <chris.d.barrett@me.com>
2014-07-28 00:42:03 +1200

    Clean up ruby conf

--- lisp/config-ruby.el
+++ lisp/config-ruby.el
@@ -45,18 +45,17 @@
 
 (add-to-list 'completion-ignored-extensions ".rbc")
 
-(-each '(("\\.rake\\'". ruby-mode)
-         ("Rakefile\\'" . ruby-mode)
-         ("\\.gemspec\\'" . ruby-mode)
-         ("\\.ru\\'" . ruby-mode)
-         ("Gemfile\\'" . ruby-mode)
-         ("Guardfile\\'" . ruby-mode)
-         ("Capfile\\'" . ruby-mode)
-         ("\\.thor\\'" . ruby-mode)
-         ("Thorfile\\'" . ruby-mode)
-         ("Vagrantfile\\'" . ruby-mode)
-         ("\\.jbuilder\\'" . ruby-mode))
-  (~ add-to-list 'auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.rake\\'". ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.thor\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Thorfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode) )
 
 (define-derived-mode erb-mode html-mode
   "ERB" nil
@@ -65,209 +64,182 @@
 
 (add-to-list 'auto-mode-alist '("\\.html\\.erb" . erb-mode))
 
-(after 'ruby-mode
+(add-hook 'ruby-mode-hook 'rvm-activate-corresponding-ruby)
 
+;;; Smart operators
+
+(defun cb-rb:rockets->colons ()
+  "Convert old-style rockets to new hash literal syntax in the current buffer."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (search-forward-regexp (rx ":" (group-n 1 (+ (not space)))
+                                      (* space)
+                                      "=>"
+                                      (* space))
+                                  nil t)
+      (replace-match "\\1: " t nil))))
+
+(defun cb-rb:smart-colon ()
+  "Insert a colon, with or without padding.
+If this is the leading colon for a symbol, do not insert padding.
+If this is the trailing colon for a hash key, insert padding."
+  (interactive)
+  (insert ":")
+  (when (s-matches? (rx (+ alnum) ":" eol)
+                    (buffer-substring (line-beginning-position) (point)))
+    (just-one-space)))
+
+(--each cb:ruby-modes
+  (super-smart-ops-configure-for-mode it
+    :add '("~")
+    :custom
+    '(("," . (command (insert ",") (just-one-space)))
+      (":" . cb-rb:smart-colon))))
+
+(after 'hideshow
+  (add-to-list 'hs-special-modes-alist
+               `(ruby-mode
+                 ,(rx (or "def" "class" "module" "{" "[")) ; Block start
+                 ,(rx (or "}" "]" "end"))                  ; Block end
+                 ,(rx (or "#" "=begin"))                   ; Comment start
+                 ruby-forward-sexp nil)))
+
+;;; HACK: Work around issues loading inf-ruby.
+
+(after 'ruby-mode
   (let ((file (f-join (-first (~ s-matches? "inf-ruby") (f-directories cb:elpa-dir))
                       "inf-ruby.el")))
 
     (autoload 'inf-ruby-mode file nil t)
-    (load-file file))
+    (load-file file)))
 
-  (after '(ruby-mode inf-ruby)
+;;; Inf-ruby interaction
 
-    (defadvice ruby-switch-to-inf (around start-inf-ruby activate)
-      "Start an inferior ruby if one is not running."
-      (condition-case _
-          ad-do-it
-        (wrong-type-argument
-         (run-ruby))))
+(defadvice ruby-switch-to-inf (around start-inf-ruby activate)
+  "Start an inferior ruby if one is not running."
+  (condition-case _
+      ad-do-it
+    (wrong-type-argument
+     (run-ruby))))
 
-    (defun set-ruby-interpreter (cmd)
-      "Set the default ruby interpreter to CMD."
-      (interactive
-       (list
-        (ido-completing-read
-         "Inferior Ruby Program: "
-         (->> inf-ruby-implementations
-           (-map 'car)
-           (-filter 'executable-find)))))
-      (setq inf-ruby-default-implementation cmd))
+(defun set-ruby-interpreter (cmd)
+  "Set the default ruby interpreter to CMD."
+  (interactive
+   (list
+    (ido-completing-read
+     "Inferior Ruby Program: "
+     (->> inf-ruby-implementations
+       (-map 'car)
+       (-filter 'executable-find)))))
+  (setq inf-ruby-default-implementation cmd))
 
-    (defun cb-rb:inf-ruby-window ()
-      (-when-let (buf (get-buffer inf-ruby-buffer))
-        (--first-window (equal (window-buffer it) buf))))
+(defun cb-rb:inf-ruby-window ()
+  (-when-let (buf (get-buffer inf-ruby-buffer))
+    (--first-window (equal (window-buffer it) buf))))
 
-    (defun restart-ruby ()
-      (interactive)
-      ;; Suppress exit query.
-      (-when-let (proc (ignore-errors (inf-ruby-proc)))
-        (set-process-query-on-exit-flag proc nil))
-      ;; Kill and relaunch IRB, reusing existing window.
-      (let ((win (cb-rb:inf-ruby-window)))
-        (ignore-errors (kill-buffer inf-ruby-buffer))
-        (save-window-excursion (run-ruby))
-        (when win
-          (set-window-buffer win inf-ruby-buffer))))
+(defun restart-ruby ()
+  (interactive)
+  ;; Suppress exit query.
+  (-when-let (proc (ignore-errors (inf-ruby-proc)))
+    (set-process-query-on-exit-flag proc nil))
+  ;; Kill and relaunch IRB, reusing existing window.
+  (let ((win (cb-rb:inf-ruby-window)))
+    (ignore-errors (kill-buffer inf-ruby-buffer))
+    (save-window-excursion (run-ruby))
+    (when win
+      (set-window-buffer win inf-ruby-buffer))))
 
-    (defun cb-rb:switch-to-ruby ()
-      "Toggle between irb and the last ruby buffer.
+(defun cb-rb:switch-to-ruby ()
+  "Toggle between irb and the last ruby buffer.
 Start an inferior ruby if necessary."
-      (interactive)
-      (cond
-       ((derived-mode-p 'inf-ruby-mode)
-        (switch-to-buffer-other-window
-         (--first-buffer (derived-mode-p 'ruby-mode))))
-       ((and inf-ruby-buffer (get-buffer inf-ruby-buffer))
-        (ruby-switch-to-inf t))
-       (t
-        (run-ruby))))
+  (interactive)
+  (cond
+   ((derived-mode-p 'inf-ruby-mode)
+    (switch-to-buffer-other-window
+     (--first-buffer (derived-mode-p 'ruby-mode))))
+   ((and inf-ruby-buffer (get-buffer inf-ruby-buffer))
+    (ruby-switch-to-inf t))
+   (t
+    (run-ruby))))
 
-    (define-key ruby-mode-map (kbd "C-c C-z") 'cb-rb:switch-to-ruby)
-    (define-key inf-ruby-mode-map (kbd "C-c C-z") 'cb-rb:switch-to-ruby)
-    (define-key inf-ruby-minor-mode-map (kbd "C-c C-z") 'cb-rb:switch-to-ruby)
+(defun cb-rb:eval-dwim ()
+  "Perform a context-sensitive evaluation."
+  (interactive)
+  ;; Start ruby if necessary.
+  (unless (get-buffer "*ruby*")
+    (run-ruby)
+    (cb-rb:switch-to-ruby)
+    ;; Revert window layout.
+    (when (= 2 (length (window-list)))
+      (delete-other-windows)))
+  (cond
+   ;; Evaluate active region.
+   ((region-active-p)
+    (ruby-send-region (region-beginning) (region-end)))
+   ;; Evaluate the block at or just before point.
+   ((or (thing-at-point-looking-at
+         (rx (or "end" "]" "}" ")") (* space) (* "\n")))
+        (ruby-block-contains-point (point)))
+    (ruby-send-block))
+   ;; Eval the block-like thing around point.
+   (t
+    (ruby-send-region (line-beginning-position)
+                      (line-end-position)))))
 
-    (defun cb-rb:eval-dwim ()
-      "Perform a context-sensitive evaluation."
-      (interactive)
-      ;; Start ruby if necessary.
-      (unless (get-buffer "*ruby*")
-        (run-ruby)
-        (cb-rb:switch-to-ruby)
-        ;; Revert window layout.
-        (when (= 2 (length (window-list)))
-          (delete-other-windows)))
-      (cond
-       ;; Evaluate active region.
-       ((region-active-p)
-        (ruby-send-region (region-beginning) (region-end)))
-       ;; Evaluate the block at or just before point.
-       ((or (thing-at-point-looking-at
-             (rx (or "end" "]" "}" ")") (* space) (* "\n")))
-            (ruby-block-contains-point (point)))
-        (ruby-send-block))
-       ;; Eval the block-like thing around point.
-       (t
-        (ruby-send-region (line-beginning-position)
-                          (line-end-position)))))
+;;; Improve font-locking for inf-ruby.
 
-    (define-key ruby-mode-map (kbd "C-c C-c") 'cb-rb:eval-dwim)
-
-    (defun cb-rb:format-irb-error (lines)
-      "Return a propertized error string for the given LINES of
+(defun cb-rb:format-irb-error (lines)
+  "Return a propertized error string for the given LINES of
 an irb error message."
-      (-when-let* ((err (--first (s-matches? "Error:" it) lines))
-                   (colon (s-index-of ":" err)))
-        (concat (propertize (substring err 0 colon) 'face 'error)
-                (substring err colon))))
+  (-when-let* ((err (--first (s-matches? "Error:" it) lines))
+               (colon (s-index-of ":" err)))
+    (concat (propertize (substring err 0 colon) 'face 'error)
+            (substring err colon))))
 
-    (defun cb-rb:apply-font-lock (str)
-      "Apply ruby font-locking to string STR."
-      (with-temp-buffer
-        (insert str)
-        (require 'ruby-mode)
-        ;; Configure ruby font-lock.
-        (set (make-local-variable 'font-lock-defaults)
-             '((ruby-font-lock-keywords) nil nil))
-        (set (make-local-variable 'syntax-propertize-function)
-             'ruby-syntax-propertize-function)
+(defun cb-rb:apply-font-lock (str)
+  "Apply ruby font-locking to string STR."
+  (with-temp-buffer
+    (insert str)
+    (require 'ruby-mode)
+    ;; Configure ruby font-lock.
+    (set (make-local-variable 'font-lock-defaults)
+         '((ruby-font-lock-keywords) nil nil))
+    (set (make-local-variable 'syntax-propertize-function)
+         'ruby-syntax-propertize-function)
 
-        (font-lock-fontify-buffer)
-        (buffer-string)))
+    (font-lock-fontify-buffer)
+    (buffer-string)))
 
-    (defun cb-rb:filter-irb-output (str &rest _)
-      "Print IRB output to messages."
-      (ignore-errors
-        (when (and (fboundp 'inf-ruby-proc) (inf-ruby-proc))
-          (let ((lines
-                 (->> (s-lines str)
-                   (--remove (or (s-contains? "--inf-ruby" it)
-                                 (s-blank? it)
-                                 (s-matches? inf-ruby-prompt-pattern it)))
-                   (-map 's-trim))))
-            (message (or (cb-rb:format-irb-error lines)
-                         (cb-rb:apply-font-lock (car (reverse lines))))))))
-      str)
+(defun cb-rb:filter-irb-output (str &rest _)
+  "Print IRB output to messages."
+  (ignore-errors
+    (when (and (fboundp 'inf-ruby-proc) (inf-ruby-proc))
+      (let ((lines
+             (->> (s-lines str)
+               (--remove (or (s-contains? "--inf-ruby" it)
+                             (s-blank? it)
+                             (s-matches? inf-ruby-prompt-pattern it)))
+               (-map 's-trim))))
+        (message (or (cb-rb:format-irb-error lines)
+                     (cb-rb:apply-font-lock (car (reverse lines))))))))
+  str)
 
-    (hook-fn 'inf-ruby-mode-hook
-      (add-hook 'comint-preoutput-filter-functions 'cb-rb:filter-irb-output)
-      ;; Stop IRB from echoing input.
-      (setq comint-process-echoes t))
+(hook-fn 'inf-ruby-mode-hook
+  (add-hook 'comint-preoutput-filter-functions 'cb-rb:filter-irb-output)
+  ;; Stop IRB from echoing input.
+  (setq comint-process-echoes t))
 
-    )
+;;; Key bindings
 
-  (defun cb-rb:rockets->colons ()
-    "Convert old-style rockets to new hash literal syntax in the current buffer."
-    (interactive)
-    (save-excursion
-      (goto-char (point-min))
-      (while (search-forward-regexp (rx ":" (group-n 1 (+ (not space)))
-                                        (* space)
-                                        "=>"
-                                        (* space))
-                                    nil t)
-        (replace-match "\\1: " t nil))))
+(after 'ruby-mode
+  (define-key ruby-mode-map (kbd "C-c C-z") 'cb-rb:switch-to-ruby)
+  (define-key ruby-mode-map (kbd "C-c C-c") 'cb-rb:eval-dwim)
+  )
 
-  (add-hook 'ruby-mode-hook 'rvm-activate-corresponding-ruby)
-
-  (add-hook 'cb:ruby-modes-hook 'subword-mode)
-
-  (defun cb-rb:smart-colon ()
-    "Insert a colon, with or without padding.
-If this is the leading colon for a symbol, do not insert padding.
-If this is the trailing colon for a hash key, insert padding."
-    (interactive)
-    (insert ":")
-    (when (s-matches? (rx (+ alnum) ":" eol)
-                      (buffer-substring (line-beginning-position) (point)))
-      (just-one-space)))
-
-  (--each cb:ruby-modes
-    (super-smart-ops-configure-for-mode it
-      :add '("~")
-      :custom
-      '(("," . (command (insert ",") (just-one-space)))
-        (":" . cb-rb:smart-colon))))
-
-  (require 'smartparens-ruby)
-
-  (modify-syntax-entry ?@ "w" ruby-mode-syntax-table)
-  (modify-syntax-entry ?_ "w" ruby-mode-syntax-table)
-  (modify-syntax-entry ?! "w" ruby-mode-syntax-table)
-  (modify-syntax-entry ?? "w" ruby-mode-syntax-table)
-
-  (defun sp-ruby-should-insert-pipe-close (_id _action _ctx)
-    "Test whether to insert the closing pipe for a lambda-binding pipe pair."
-    (thing-at-point-looking-at
-     (rx-to-string `(and (or "do" "{") (* space) "|"))))
-
-  (defun sp-ruby-sp-hook-space-before (_id action _ctx)
-    "Move to point before ID and insert a space."
-    (when (equal 'insert action)
-      (save-excursion
-        (search-backward "|")
-        (just-one-space))))
-
-  (defun sp-ruby-sp-hook-space-after (_id action _ctx)
-    "Move to point after ID and insert a space."
-    (when (equal 'insert action)
-      (save-excursion
-        (search-forward "|")
-        (just-one-space))))
-
-  (hook-fn 'cb:ruby-modes-hook
-    (local-set-key (kbd "C-c C-h") 'yari))
-
-  (after 'hideshow
-    (add-to-list 'hs-special-modes-alist
-                 `(ruby-mode
-                   ,(rx (or "def" "class" "module" "{" "[")) ; Block start
-                   ,(rx (or "}" "]" "end"))                  ; Block end
-                   ,(rx (or "#" "=begin"))                   ; Comment start
-                   ruby-forward-sexp nil)))
-
-  (after 'evil
-    (define-evil-doc-handler cb:ruby-modes (call-interactively 'robe-doc)))
-
+(after 'inf-ruby
+  (define-key inf-ruby-mode-map (kbd "C-c C-z") 'cb-rb:switch-to-ruby)
+  (define-key inf-ruby-minor-mode-map (kbd "C-c C-z") 'cb-rb:switch-to-ruby)
   )
 
 (provide 'config-ruby)
