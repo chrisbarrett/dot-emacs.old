fb3445bf57fbcc413ea27e3b6881dd5c0d9df03f
Chris Barrett <chris.d.barrett@me.com>
2014-08-12 12:17:13 +1200

    [evil] fix loading issue

--- lisp/config-evil.el
+++ lisp/config-evil.el
@@ -611,22 +611,22 @@ Runs each handler added to `evil-find-doc-hook' until one of them returns non-ni
 (evil-set-initial-state 'geiser-repl-mode 'insert)
 
 ;;; w3m
-
-(bind-keys
-  :map w3m-mode-map
-  "z t" 'evil-scroll-line-to-top
-  "z b" 'evil-scroll-line-to-bottom
-  "z z" 'evil-scroll-line-to-center
-  "C-f" 'evil-scroll-page-down
-  "C-b" 'evil-scroll-page-up
-  "w"   'evil-forward-word-begin
-  "b"   'evil-backward-word-begin
-  "y"   'evil-yank
-  "p"   'evil-paste-after
-  "/"   'evil-search-forward
-  "?"   'evil-search-backward
-  "n"   'evil-search-next
-  "N"   'evil-search-previous)
+(after 'w3m
+  (bind-keys
+    :map w3m-mode-map
+    "z t" 'evil-scroll-line-to-top
+    "z b" 'evil-scroll-line-to-bottom
+    "z z" 'evil-scroll-line-to-center
+    "C-f" 'evil-scroll-page-down
+    "C-b" 'evil-scroll-page-up
+    "w"   'evil-forward-word-begin
+    "b"   'evil-backward-word-begin
+    "y"   'evil-yank
+    "p"   'evil-paste-after
+    "/"   'evil-search-forward
+    "?"   'evil-search-backward
+    "n"   'evil-search-next
+    "N"   'evil-search-previous))
 
 ;;; Misc
 
