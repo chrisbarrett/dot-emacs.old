fa877764828359f05b1381211b6136fd9bcfb4b2
Chris Barrett <chris.d.barrett@me.com>
2014-11-23 20:41:47 +1300

    Iedit improvements

--- lisp/config-iedit.el
+++ lisp/config-iedit.el
@@ -41,11 +41,39 @@
     (iedit-goto-first-occurrence)
     (iedit-replace-occurrences (read-string "Replace in buffer: "))))
 
+(defun cbiedit:number-of-occurrences ()
+  "Return the number of active iedit occurrences."
+  (save-excursion
+    (goto-char (point-min))
+    (let ((n 0))
+      (until (eobp)
+        (let ((pos (point))
+              (in-occurrence (get-char-property (point) 'iedit-occurrence-overlay-name)))
+          (when in-occurrence
+            (setq pos (next-single-char-property-change pos 'iedit-occurrence-overlay-name)))
+          (setq pos (next-single-char-property-change pos 'iedit-occurrence-overlay-name))
+          (goto-char pos)
+          (cl-incf n)))
+      (1- n))))
+
 (defun cbiedit:restrict-to-region ()
-  (iedit-restrict-region (region-beginning) (region-end)))
+  (iedit-restrict-region (region-beginning) (region-end))
+  (message "Restricted to %s matches in region."
+           (cbiedit:number-of-occurrences)))
 
 (defun cbiedit:remove-region ()
   (iedit-restrict-region (region-beginning) (region-end) t))
+
+(defun cbiedit:restrict-to-window ()
+  (let ((top (save-excursion
+               (move-to-window-line 0)
+               (point)))
+        (bottom (save-excursion
+                  (move-to-window-line -1)
+                  (point))))
+    (iedit-restrict-region top bottom)
+    (message "Restricted to %s matches in visible area."
+             (cbiedit:number-of-occurrences))))
 
 (defun cbiedit:replace-in-region ()
   (cbiedit:restrict-to-region)
@@ -65,6 +93,7 @@
 
     ("l" "Restrict (line)" iedit-restrict-current-line)
     ("R" "Restrict (region)" cbiedit:restrict-to-region :when region-active-p)
+    ("w" "Restrict (window)" cbiedit:restrict-to-window)
     ("x" "Remove (region)" cbiedit:remove-region :when region-active-p)
     ("f" "Restrict (function)" iedit-restrict-function :when (lambda () (thing-at-point 'defun)))
 
